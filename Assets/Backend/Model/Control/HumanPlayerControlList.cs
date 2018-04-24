72e4ed9bad5c9cf3a89b7918466fc6c1781b48c2
Unknown <miquelaulamart@gmail.com>
2018-04-24 22:09:51 +0200

    Modification of World1v2 and adding joysticks controllers to P1
    Needs to fix jump and adding input axis for the controller.

--- Assets/Backend/Model/Control/HumanPlayerControlList.cs
+++ Assets/Backend/Model/Control/HumanPlayerControlList.cs
@@ -13,20 +13,31 @@ namespace Assets.Backend.Model.Input
         {
             Controls = new List<HumanPlayerControl>
             {
+//                new HumanPlayerControl
+//                {
+//                    PlayerInputSource = EnPlayerInputSource.PlayerOne,
+//                    Input = new PlayerInput {
+//                        KeyDuck = KeyCode.DownArrow,
+//                        KeyJump = KeyCode.UpArrow,
+//                        KeyMoveLeft = KeyCode.LeftArrow,
+//                        KeyMoveRight = KeyCode.RightArrow,
+//                        KeyPossess = KeyCode.L,
+//                        KeyDash = KeyCode.J
+//                    },
+//                    IsActive = true
+//                },
                 new HumanPlayerControl
                 {
                     PlayerInputSource = EnPlayerInputSource.PlayerOne,
                     Input = new PlayerInput {
-                        KeyDuck = KeyCode.DownArrow,
-                        KeyJump = KeyCode.UpArrow,
-                        KeyMoveLeft = KeyCode.LeftArrow,
-                        KeyMoveRight = KeyCode.RightArrow,
-                        KeyPossess = KeyCode.L,
-                        KeyDash = KeyCode.J
+                        KeyDuck = KeyCode.Joystick1Button1,
+                        KeyJump = KeyCode.Joystick1Button0,
+                        KeyMoveLeft = KeyCode.Joystick1Button4,
+                        KeyMoveRight = KeyCode.Joystick1Button5,
+                        KeyPossess = KeyCode.Joystick1Button2,
+                        KeyDash = KeyCode.Joystick1Button3
                     },
                     IsActive = true
-
-                    
                 },
                 new HumanPlayerControl
                 {
@@ -38,7 +49,18 @@ namespace Assets.Backend.Model.Input
                         KeyMoveRight = KeyCode.D
                     },
                     IsActive = true
-                }
+                },
+//                new HumanPlayerControl
+//                {
+//                    PlayerInputSource = EnPlayerInputSource.PlayerTwo,
+//                    Input = new PlayerInput {
+//                        KeyDuck = KeyCode.,
+//                        KeyJump = KeyCode.,
+//                        KeyMoveLeft = KeyCode.,
+//                        KeyMoveRight = KeyCode.
+//                    },
+//                    IsActive = true
+//                }
             };
         }
 
@@ -48,3 +70,30 @@ namespace Assets.Backend.Model.Input
         }
     }
 }
+
+
+/*
+ * Inputs joystick cotroller 
+A 								-> joystick button 0
+B 								-> joystick button 1
+X 								-> joystick button 2
+Y 								-> joystick button 3
+Left Bumper 					-> joystick button 4
+Right Bumper 					-> joystick button 5
+View (Back) 					-> joystick button 6
+Menu (Start) 					-> joystick button 7
+Left Stick Button 				-> joystick button 8
+Right Stick Button 				-> joystick button 9
+Left Stick “Horizontal” 		-> X Axis 		-> -1 to 1
+Left Stick “Vertical” 			-> Y Axis		-> 1 to -1
+Right Stick “HorizontalTurn” 	-> 4th Axis 	-> -1 to 1
+Right Stick “VerticalTurn” 		-> 5th Axis 	-> 1 to -1
+DPAD – Horizontal 				-> 6th Axis 	-> -1 (.64) 1
+DPAD – Vertical 				-> 7th Axis 	-> -1 (.64) 1
+Left Trigger 					-> 9th Axis 	-> 0 to 1
+Right Trigger 					-> 10th Axis 	-> 0 to 1
+Left Trigger Shared Axis 		-> 3rd Axis 	-> 0 to 1
+Right Trigger Shared Axis 		-> 3rd Axis 	-> 0 to -1
+ * 
+ */
+ 
